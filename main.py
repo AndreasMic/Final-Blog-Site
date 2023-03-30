669dfc6f673c5f939f76165bc07c25d25f97d799
Andreas Michail <andmi93@gmail.com>
2023-03-30 17:16:36 +0300

    Hidden env commit

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 ##INITIALIZE THE APP
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("APP_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
