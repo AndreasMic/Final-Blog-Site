8aaeb226879da2bb2cbab58ca3497d0109e42a4f
Andreas Michail <andmi93@gmail.com>
2023-03-31 06:15:28 +0300

    Sans login_required

--- main.py
+++ main.py
@@ -146,7 +146,6 @@ def logout():
 
 
 @app.route("/post/<int:post_id>", methods=['GET', 'POST'])
-@login_required
 def show_post(post_id):
     requested_post = BlogPost.query.get(post_id)
     form = CommentForm()
